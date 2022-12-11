ef53c1bf784c2f1b018409daf62cc7fcd5188814
NeelamK1990 <neelamkatwal606@gmail.com>
2022-12-11 11:09:09 -0500

    Added live data for New York and Hanoi also updated css and html

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,21 @@
+function updateTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  let newYorkTime = moment().tz("America/New_York");
 
+  newYorkDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = newYorkTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let hanoiElement = document.querySelector("#hanoi");
+  let hanoiDateElement = hanoiElement.querySelector(".date");
+  let hanoiTimeElement = hanoiElement.querySelector(".time");
+  let hanoiTime = moment().tz("Asia/Saigon");
+
+  hanoiDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  hanoiTimeElement.innerHTML = hanoiTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime, 1000);
