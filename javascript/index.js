e29ecb59c3d971616d8d17e4d7e1c157cdea0c9d
NeelamK1990 <neelamkatwal606@gmail.com>
2022-12-11 14:33:50 -0500

    Added function for select

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,48 @@
 function updateTime() {
   let newYorkElement = document.querySelector("#new-york");
-  let newYorkDateElement = newYorkElement.querySelector(".date");
-  let newYorkTimeElement = newYorkElement.querySelector(".time");
-  let newYorkTime = moment().tz("America/New_York");
+  if (newYorkElement) {
+    let newYorkDateElement = newYorkElement.querySelector(".date");
+    let newYorkTimeElement = newYorkElement.querySelector(".time");
+    let newYorkTime = moment().tz("America/New_York");
 
-  newYorkDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  newYorkTimeElement.innerHTML = newYorkTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    newYorkDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    newYorkTimeElement.innerHTML = newYorkTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let hanoiElement = document.querySelector("#hanoi");
-  let hanoiDateElement = hanoiElement.querySelector(".date");
-  let hanoiTimeElement = hanoiElement.querySelector(".time");
-  let hanoiTime = moment().tz("Asia/Saigon");
+  if (hanoiElement) {
+    let hanoiDateElement = hanoiElement.querySelector(".date");
+    let hanoiTimeElement = hanoiElement.querySelector(".time");
+    let hanoiTime = moment().tz("Asia/Saigon");
 
-  hanoiDateElement.innerHTML = moment().format("MMMM Do YYYY");
-  hanoiTimeElement.innerHTML = hanoiTime.format("h:mm:ss [<small>]A[</small>]");
+    hanoiDateElement.innerHTML = moment().format("MMMM Do YYYY");
+    hanoiTimeElement.innerHTML = hanoiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
+
+function changeCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss "
+          )}<small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelect = document.querySelector("#city");
+
+citiesSelect.addEventListener("change", changeCity);
